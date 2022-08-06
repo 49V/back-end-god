c30619a019619fda8d2c31c24f6a511f38ad1b91
Moshe Lawlor <moshelawlor@Moshes-MacBook-Pro.local>
2022-08-06 15:42:38 -0700

    Chuck testa

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const port = 3000;
 app.get("/", function (req, res) {
   res.send("Hello World");
   const unusedVariable = 1
+   const unsuedVariableTow = 2
 });
 
 console.log(`Listening on port ${port}`);
